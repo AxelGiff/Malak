40ee03f29d5927eea6c43c42a2d9f8674e93c831
axel <giffard.axel95@gmail.com>
2026-05-21 22:49:41 +0200

    FEAT: sending metrics from agent to api with reqwest POST & tokio

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+use serde::Serialize;
+use sysinfo::System;
+
+#[derive(Debug, Serialize)]
+struct Metrics {
+    cpu: f32,
+    ram: u64,
+}
+
+#[tokio::main]
+async fn main() {
+    let mut system = System::new_all();
+
+    system.refresh_all();
+
+    let metrics = Metrics {
+        cpu: system.global_cpu_usage(),
+        ram: system.used_memory(),
+    };
+    loop{
+    send_metrics(&metrics).await;
+     tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
+  
+}
+
+async fn send_metrics(metrics: &Metrics){
+  let client = Client::new();
+
+    let response = client
+        .post("http://localhost:3000/metrics")
+        .json(metrics)
+        .send()
+        .await
+        .unwrap();
+
+    println!("Status: {}", response.status());
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,23 @@
 use axum::{
     routing::get,
+    routing::post,
     Router,
+    Json
 };
+use serde_json::Value;
 
+async fn receive_metrics(
+    Json(payload): Json<Value>,
+) {
+    println!("Received metrics:");
+    println!("{:?}", payload);
+}
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+    .route("/", get(|| async { "Hello, World!" }))
+    .route("/metrics", post(receive_metrics));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
